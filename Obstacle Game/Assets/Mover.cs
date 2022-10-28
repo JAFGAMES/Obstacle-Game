6fed2235b7016f4c4523c0b93db31e6ea39ff2cc
JAFGAMES <653497@student.cityofbristol.ac.uk>
2022-10-28 20:18:47 +0100

    Got the cube moving

--- Obstacle Game/Assets/Mover.cs
+++ Obstacle Game/Assets/Mover.cs
@@ -2,23 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mover : MonoBehaviour
+public class Mover : MonoBehaviour
 {
-    public float xValue = 1000f;
-    public float yValue = 0.5f;
-    public float zValue = 1000f;
+    [SerializeField] float moveSpeed5 = 5.0f;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+			PrintInstructions(); //We are calling the method, here
     }
 
     // Update is called once per frame
     void Update()
     {
-        //public float xValue = 0f;
-        //public float yValue = 0f;
-        transform.Translate(xValue, 0, zValue);
+		MovePlayer();
     }
+
+
+		void PrintInstructions () 
+
+		{
+			Debug.Log("Welcome to the game");
+			Debug.Log("Move your player with the WASD or arrow keys");
+			Debug.Log("Don't hit the walls");
+
+		}
+        
+
+        void MovePlayer () 
+        {
+            float xValue = Input.GetAxis("Horizontal")* Time.deltaTime* moveSpeed5;
+            float zValue = Input.GetAxis("Vertical")* Time.deltaTime* moveSpeed5;
+            transform.Translate(xValue,0,zValue);
+            Debug.Log("The Value of xValue is " + xValue);
+            Debug.Log("The Value of zValue is " + zValue);
+            Debug.Log("The Value of Time.deltaTime is " + Time.deltaTime);
+            Debug.Log("The Value of moveSpeed5 is " + moveSpeed5);
+        }
+
+
 }
