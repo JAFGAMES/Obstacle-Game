87a2c663fa12af693dfe1031135fef33e5b3afc0
JAFGAMES <653497@student.cityofbristol.ac.uk>
2022-10-30 18:13:09 +0000

    V2.1-HB-UI-Working

--- Obstacle Game/Assets/Scripts/PlayerBehaviour.cs
+++ Obstacle Game/Assets/Scripts/PlayerBehaviour.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
+    [SerializeField] Healthbar _healthbar;
 
     void Start()
     {
@@ -28,10 +29,13 @@ public class PlayerBehaviour : MonoBehaviour
     private void PlayerTakeDmg(int dmg)
     {
         GameManager.gameManager._playerHealth.DmgUnit(dmg);
+        _healthbar.SetHealth(GameManager.gameManager._playerHealth.Health);
     }
 
     private void PlayerHeal(int healing)
     {
         GameManager.gameManager._playerHealth.HealUnit(healing);
+        _healthbar.SetHealth(GameManager.gameManager._playerHealth.Health);
+
     }
 }
